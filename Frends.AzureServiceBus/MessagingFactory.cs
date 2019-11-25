2bb497f4eebb8a12a67e6ccd93d31dac409cf286
Erkka Honkavaara <erkka.honkavaara@hiq.fi>
2019-11-25 10:47:01 +0200

    Initial commit based on Frends.ServiceBus: https://github.com/FrendsPlatform/Frends.ServiceBus Provides .NET Standard support

---
+++ Frends.AzureServiceBus/MessagingFactory.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.ServiceBus.Core;
+
+namespace Frends.AzureServiceBus
+{
+    /// <summary>
+    /// Class handles clients for the service bus. Enables cached connections to the service bus.
+    /// </summary>
+    public sealed class ServiceBusMessagingFactory : IDisposable
+    {
+        private static readonly Lazy<ServiceBusMessagingFactory> instanceHolder = new Lazy<ServiceBusMessagingFactory>(() => new ServiceBusMessagingFactory());
+        /// <summary>
+        /// The ServiceBusMessagingFactory singleton instance
+        /// </summary>
+        public static ServiceBusMessagingFactory Instance
+        {
+            get { return instanceHolder.Value; }
+        }
+
+        private static readonly object factoryLock = new Object();
+        private readonly ConcurrentDictionary<string, ServiceBusConnection> _connections = new ConcurrentDictionary<string, ServiceBusConnection>();
+
+        private ServiceBusMessagingFactory()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Create message receiver for the given connection string and entity path
+        /// </summary>
+        /// <param name="connectionString">Connection string</param>
+        /// <param name="path">Name of the queue</param>
+        /// <param name="timeout">TimeoutSeconds</param>
+        /// <returns></returns>
+        public MessageReceiver GetMessageReceiver(string connectionString, string path, TimeSpan timeout)
+        {
+            var receiver = new MessageReceiver(GetCachedMessagingFactory(connectionString, timeout), path, receiveMode:ReceiveMode.ReceiveAndDelete);
+
+            return receiver;
+        }
+
+        /// <summary>
+        /// Create a message sender for the given connection string and entity path
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="path"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public MessageSender GetMessageSender(string connectionString, string path, TimeSpan timeout)
+        {
+            return new MessageSender(GetCachedMessagingFactory(connectionString, timeout), path);
+        }
+
+        private ServiceBusConnection GetCachedMessagingFactory(string connectionString, TimeSpan timeout)
+        {
+            string key = timeout.TotalSeconds + "-" + connectionString;
+
+            if (!_connections.ContainsKey(key))
+            {
+                lock (factoryLock) // TODO: change double check
+                {
+                    if (!_connections.ContainsKey(key))
+                    {
+                        _connections.TryAdd(key, CreateConnectionWithTimeout(connectionString, timeout));
+                    }
+                }
+            }
+
+            return _connections[key];
+        }
+
+        /// <summary>
+        /// Create new client for servicebus connection. This method is slow!
+        /// </summary>
+        /// <param name="connectionString">Connection string</param>
+        /// <param name="operationTimeoutForClients">Operation timeout for clients</param>
+        /// <returns>Object that can handle messaging to the service bus</returns>
+        internal static ServiceBusConnection CreateConnectionWithTimeout(string connectionString, TimeSpan operationTimeoutForClients)
+        {
+            var connBuilder = new ServiceBusConnectionStringBuilder(connectionString)
+            {
+                OperationTimeout = operationTimeoutForClients
+            };
+
+            var connection = new ServiceBusConnection(connBuilder) {RetryPolicy = RetryPolicy.Default};
+
+            return connection;
+        }
+
+
+        #region IDisposable Support
+        private bool _disposedValue = false; // To detect redundant calls
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                var factoriesToClose = _connections.ToList();
+                _connections.Clear();
+
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                foreach (var item in factoriesToClose)
+                {
+                    try
+                    {
+                        item.Value.CloseAsync().Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError("Error when aborting messaging factory connection " + ex);
+                    }
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        /// <summary>Allows an object to try to free resources and perform other cleanup operations before it is reclaimed by garbage collection.</summary>
+        //~ServiceBusMessagingFactory()
+        //{
+        //    // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //    //Dispose(false);
+        //}
+
+        /// <summary>
+        /// Dispose of the MessagingFactory and close all the cached connections
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+
+            //GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
---
+++ Frends.AzureServiceBus/ServiceBus.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Azure.ServiceBus.InteropExtensions;
+using Microsoft.Azure.ServiceBus.Management;
+
+
+namespace Frends.AzureServiceBus
+{
+    /// <summary>
+    /// FRENDS ServiceBus tasks
+    /// </summary>
+    public class ServiceBus
+    {
+        private static async Task<ReadResult> DoReadOperation(string connectionString, string path, TimeSpan timeout, bool useCached, Func<MessageReceiver, Task<ReadResult>> operation)
+        {
+            ServiceBusConnection connection = null;
+            MessageReceiver requestClient = null;
+            try
+            {
+                if (useCached)
+                {
+                    requestClient = ServiceBusMessagingFactory.Instance.GetMessageReceiver(connectionString, path, timeout);
+                }
+                else
+                {
+                    connection = ServiceBusMessagingFactory.CreateConnectionWithTimeout(connectionString, timeout);
+                    requestClient = new MessageReceiver(connection, path);
+                }
+                return await operation.Invoke(requestClient).ConfigureAwait(false);
+            }
+            finally
+            {
+
+                await (requestClient?.CloseAsync() ?? Task.CompletedTask);
+
+                await (connection?.CloseAsync() ?? Task.CompletedTask);
+            }
+        }
+
+        private static async Task<SendResult> DoQueueSendOperation(string connectionString, string path, TimeSpan timeout, bool useCached, Func<MessageSender, Task<SendResult>> operation)
+        {
+            ServiceBusConnection connection = null;
+            MessageSender requestClient = null;
+            try
+            {
+                if (useCached)
+                {
+                    requestClient = ServiceBusMessagingFactory.Instance.GetMessageSender(connectionString, path, timeout);
+                }
+                else
+                {
+                    connection = ServiceBusMessagingFactory.CreateConnectionWithTimeout(connectionString, timeout);
+                    requestClient = new MessageSender(connection, path);
+                }
+                return await operation.Invoke(requestClient).ConfigureAwait(false);
+            }
+            finally
+            {
+                await (requestClient?.CloseAsync() ?? Task.CompletedTask);
+                await (connection?.CloseAsync() ?? Task.CompletedTask);
+            }
+        }
+
+        private static Encoding GetEncoding(MessageEncoding encodingChoice, string encodingName)
+        {
+            switch (encodingChoice)
+            {
+                case MessageEncoding.ASCII:
+                    return Encoding.ASCII;
+                case MessageEncoding.UTF8:
+                    return Encoding.UTF8;
+                case MessageEncoding.Unicode:
+                    return Encoding.Unicode;
+                case MessageEncoding.UTF32:
+                    return Encoding.UTF32;
+                case MessageEncoding.Other:
+                default:
+                    return Encoding.GetEncoding(encodingName);
+            }
+        }
+
+        /// <summary>
+        /// Send data to the Service Bus, don't wait for a reply. See https://github.com/FrendsPlatform/Frends.ServiceBus
+        /// </summary>
+        /// <returns>Object: {MessageId, SessionId, ContentType}</returns>
+        public static async Task<SendResult> Send([PropertyTab]SendInput input, [PropertyTab]SendOptions options, CancellationToken cancellationToken = new CancellationToken())
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (options.CreateQueueOrTopicIfItDoesNotExist)
+            {
+                switch (options.DestinationType)
+                {
+                    case QueueOrTopic.Queue:
+                        await EnsureQueueExists(input.QueueOrTopicName, input.ConnectionString).ConfigureAwait(false);
+                        break;
+                    case QueueOrTopic.Topic:
+                        await EnsureTopicExists(input.QueueOrTopicName, input.ConnectionString).ConfigureAwait(false);
+                        break;
+                    default:
+                        throw new Exception($"Unexpected destination type: {options.DestinationType}");
+                }
+            }
+
+            return await DoQueueSendOperation(input.ConnectionString, input.QueueOrTopicName, TimeSpan.FromSeconds(options.TimeoutSeconds), options.UseCachedConnection,
+                async client =>
+                {
+                    var result = new SendResult();
+
+                    byte[] body = CreateBody(input, options);
+
+                    var message = new Message(body);
+                    message.MessageId = string.IsNullOrEmpty(options.MessageId)
+                    ? Guid.NewGuid().ToString()
+                    : options.MessageId;
+                    result.MessageId = message.MessageId;
+
+                    message.SessionId = string.IsNullOrEmpty(options.SessionId) ? message.SessionId : options.SessionId;
+                    result.SessionId = message.SessionId;
+
+                    message.ContentType = options.ContentType;
+                    result.ContentType = message.ContentType;
+
+
+                    message.CorrelationId = options.CorrelationId;
+                    message.ReplyToSessionId = options.ReplyToSessionId;
+                    message.ReplyTo = options.ReplyTo;
+                    message.To = options.To;
+                    message.TimeToLive = options.TimeToLiveSeconds.HasValue
+                        ? TimeSpan.FromSeconds(options.TimeToLiveSeconds.Value)
+                        : TimeSpan.MaxValue;
+                    message.ScheduledEnqueueTimeUtc = options.ScheduledEnqueueTimeUtc;
+
+                    foreach (var property in input.Properties)
+                    {
+                        message.UserProperties.Add(property.Name, property.Value);
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    await client.SendAsync(message).ConfigureAwait(false);
+
+                    return result;
+                }).ConfigureAwait(false);
+        }
+
+        private static async Task EnsureQueueExists(string queueName, string connectionString)
+        {
+            var manager = new ManagementClient(connectionString);
+
+            if (!await manager.QueueExistsAsync(queueName).ConfigureAwait(false))
+            {
+                var queueDescription = new QueueDescription(queueName)
+                {
+                    EnableBatchedOperations = true,
+                    MaxSizeInMB = 5 * 1024,
+                    AutoDeleteOnIdle = TimeSpan.FromDays(7)
+                };
+                await manager.CreateQueueAsync(queueDescription).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task EnsureTopicExists(string topicName, string connectionString, ManagementClient managementClient = null)
+        {
+            var manager = managementClient ?? new ManagementClient(connectionString);
+
+            if (!await manager.TopicExistsAsync(topicName).ConfigureAwait(false))
+            {
+                var topicDescription = new TopicDescription(topicName)
+                {
+                    EnableBatchedOperations = true,
+                    MaxSizeInMB = 5 * 1024,
+                    AutoDeleteOnIdle = TimeSpan.FromDays(7)
+                };
+                await manager.CreateTopicAsync(topicDescription).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task EnsureSubscriptionExists(string topicName, string subscriptionName, string connectionString)
+        {
+            var manager = new ManagementClient(connectionString);
+
+            await EnsureTopicExists(topicName, connectionString, manager).ConfigureAwait(false);
+
+            if (!await manager.SubscriptionExistsAsync(topicName, subscriptionName).ConfigureAwait(false))
+            {
+                var subscriptionDescription = new SubscriptionDescription(topicName, subscriptionName)
+                {
+                    EnableBatchedOperations = true,
+                    AutoDeleteOnIdle = TimeSpan.FromDays(7)
+                };
+                await manager.CreateSubscriptionAsync(subscriptionDescription).ConfigureAwait(false);
+            }
+        }
+
+        private static byte[] CreateBody(SendInput input, SendOptions options)
+        {
+            if (input.Data == null)
+            {
+                return null;
+            }
+
+            var contentTypeString = options.ContentType;
+
+            var encoding = GetEncodingFromContentType(contentTypeString, Encoding.UTF8);
+
+            // This format matches the older Frends.ServiceBus format to ensure interoperability
+            switch (options.BodySerializationType)
+            {
+                case BodySerializationType.String:
+                {
+                    return SerializeObject<string>(input.Data);
+                }
+
+                case BodySerializationType.ByteArray:
+                {
+                    return SerializeObject<byte[]>(encoding.GetBytes(input.Data));
+                }
+                    
+                case BodySerializationType.Stream:
+                default:
+                    return encoding.GetBytes(input.Data);
+            }
+        }
+
+        private static byte[] SerializeObject<T>(object serializableObject)
+        {
+            var serializer = DataContractBinarySerializer<T>.Instance;
+            if (serializableObject == null)
+                return null;
+
+            using MemoryStream memoryStream = new MemoryStream(256);
+            serializer.WriteObject(memoryStream, serializableObject);
+            memoryStream.Flush();
+            memoryStream.Position = 0L;
+            return memoryStream.ToArray();
+        }
+
+        private static Encoding GetEncodingFromContentType(string contentTypeString, Encoding defaultEncoding)
+        {
+            Encoding encoding = defaultEncoding;
+            if (!string.IsNullOrEmpty(contentTypeString))
+            {
+                var contentType = new ContentType(contentTypeString);
+                if (!string.IsNullOrEmpty(contentType.CharSet))
+                {
+                    encoding = Encoding.GetEncoding(contentType.CharSet);
+                }
+            }
+            return encoding;
+        }
+
+        /// <summary>
+        /// Read a message from the Service Bus. See https://github.com/FrendsPlatform/Frends.ServiceBus
+        /// </summary>
+        /// <param name="input">Input parameters</param>
+        /// <param name="options">Option parameters</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Object {ReceivedMessage(boolean), ContentType, SessionId, MessageId, CorrelationId, DeliveryCount, EnqueuedSequenceNumber, SequenceNumber, Label, Properties(dictionary), ReplyTo, ReplyToSessionId, Size, State, To, Content}</returns>
+        public static async Task<ReadResult> Read([PropertyTab]ReadInput input, [PropertyTab]ReadOptions options,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (options.CreateQueueOrSubscriptionIfItDoesNotExist)
+            {
+                switch (input.SourceType)
+                {
+                    case QueueOrSubscription.Queue:
+                        await EnsureQueueExists(input.QueueOrTopicName, input.ConnectionString);
+                        break;
+                    case QueueOrSubscription.Subscription:
+                        await EnsureSubscriptionExists(input.QueueOrTopicName, input.SubscriptionName, input.ConnectionString);
+                        break;
+                    default:
+                        throw new Exception($"Unexpected destination type: {input.SourceType}");
+                }
+            }
+
+            var path = input.QueueOrTopicName;
+            if (input.SourceType == QueueOrSubscription.Subscription)
+            {
+                path = $"{input.QueueOrTopicName}/subscriptions/{input.SubscriptionName}";
+            }
+
+            return await DoReadOperation(input.ConnectionString, path, TimeSpan.FromSeconds(options.TimeoutSeconds),
+                options.UseCachedConnection,
+                async client =>
+                {
+                    var msg = await client.ReceiveAsync().ConfigureAwait(false);
+
+                    if (msg == null)
+                    {
+                        return new ReadResult
+                        {
+                            ReceivedMessage = false
+                        };
+                    }
+
+                    var result = new ReadResult
+                    {
+                        ReceivedMessage = true,
+                        ContentType = msg.ContentType,
+                        Properties = msg.UserProperties?.ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                        SessionId = msg.SessionId,
+                        MessageId = msg.MessageId,
+                        CorrelationId = msg.CorrelationId,
+                        Label = msg.Label,
+                        DeliveryCount = msg.SystemProperties.DeliveryCount,
+                        EnqueuedSequenceNumber = msg.SystemProperties.EnqueuedSequenceNumber,
+                        SequenceNumber = msg.SystemProperties.SequenceNumber,
+                        ReplyTo = msg.ReplyTo,
+                        ReplyToSessionId = msg.ReplyToSessionId,
+                        Size = msg.Size,
+                        //State = msg.State.ToString(), // State
+                        To = msg.To,
+                        ScheduledEnqueueTimeUtc = msg.ScheduledEnqueueTimeUtc,
+                        Content = await ReadMessageBody(msg, options).ConfigureAwait(false)
+                    };
+                    return result;
+                }).ConfigureAwait(false);
+        }
+
+        private static async Task<string> ReadMessageBody(Message msg, ReadOptions options)
+        {
+            // Body is a string
+            if (options.BodySerializationType == BodySerializationType.String)
+            {
+                return msg.GetBody<string>();
+            }
+
+            Encoding encoding = GetEncodingFromContentType(msg.ContentType, GetEncoding(options.DefaultEncoding, options.EncodingName));
+
+            // Body is a byte array
+            if (options.BodySerializationType == BodySerializationType.ByteArray || options.BodySerializationType == BodySerializationType.Stream)
+            {
+                // Stream and byte arrays handled the same way, option kept in for parity with old Frends.ServiceBus task
+                var messageBytes = msg.Body;
+                return messageBytes == null ? null : encoding.GetString(messageBytes);
+            }
+
+            throw new ArgumentException($"Unsupported BodySerializationType: {options.BodySerializationType}");
+        }
+    }
+
+
+
+}
